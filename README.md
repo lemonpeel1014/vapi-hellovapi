a63bc2db095de583ba66c55b4913d78bd146aeb2
lemonpeel1014 <jdtyy123@gmail.com>
2025-02-06 18:11:15 +0900

    init project

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # vapi-hellovapi
+hellovapi VAPI
+
+## Environment Variables
+- `SHAPLE_URL`: Shaple URL
+- `SHAPLE_SERVICE_KEY`: Shaple Service Key
---
+++ hello_vapi/index.ts
@@ -0,0 +1,8 @@
+Deno.serve(async (req) => {
+  const { name } = await req.json();
+  const data = {
+    message: `Hello ${name}!`,
+  }
+
+  return new Response(JSON.stringify(data), { headers: { 'Content-Type': 'application/json' } })
+})
